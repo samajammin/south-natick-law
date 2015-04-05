4fe262639b50656f372eb84fc654682252f0e224
Sam Richards <sbrichards@gmail.com>
2015-04-04 18:38:29 -0700

    Add 404.html and set TEMPLATE_DIRS in settings.py

--- snl/snl/settings.py
+++ snl/snl/settings.py
@@ -114,6 +114,10 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__fi
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "static", *MEDIA_URL.strip("/").split("/"))
 
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'website/templates/'),
+)
+
 try:
     from local_settings import *
 except ImportError:

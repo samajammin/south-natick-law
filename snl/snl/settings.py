ea783dd1db425717756254dc58f72992e17e00d1
Sam Richards <sbrichards@gmail.com>
2015-04-15 22:16:29 -0700

    Fix media file reference

--- snl/snl/settings.py
+++ snl/snl/settings.py
@@ -117,7 +117,6 @@ TEMPLATE_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-
 # STATIC_ROOT = os.path.join(BASE_DIR, "..", "snl", "website", "static")
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'

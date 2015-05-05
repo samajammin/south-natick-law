ecc693ccf0535465dbe07251f0cb1361e029afe5
Sam Richards <sbrichards@gmail.com>
2015-05-05 16:10:49 -0700

    Fix canonicals

--- snl/snl/urls.py
+++ snl/snl/urls.py
@@ -13,13 +13,9 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
     url(r'^$', 'website.views.home', name='home'),
     url(r'^about$', 'website.views.about', name='about'),
-
-    # catch name as a parameter & pass into view for url assignment
+    url(r'^about/the-attorneys$', 'website.views.about', name='about'),
+    url(r'^about/the-firm$', 'website.views.about', name='about'),
     url(r'^about/(?P<slug>\S+)$', 'website.views.profile', name='profile'),
-    # url(r'^about/james', 'website.views.james', name='james'),
-    # url(r'^about/richard', 'website.views.richard', name='richard'),
-    # url(r'^about/caren', 'website.views.caren', name='caren'),
-    # url(r'^about/george', 'website.views.george', name='george'),
 
     url(r'^areas-of-practice$', 'website.views.aop', name='aop'),
 
@@ -29,18 +25,13 @@ urlpatterns = patterns('',
 
     # logic to match specific resources posts first
     url(r'^blog$', 'website.views.blog', name='blog'),
-    # url(r'^blog/(?P<post_id>\w+)/$', 'website.views.view_post', name='view_post'),
-
     url(r'^blog/author/(?P<slug>\S+)$', 'website.views.author_index', name='author_index'),
     url(r'^blog/tag/(?P<slug>\S+)$', 'website.views.tag_index', name='tag_index'),
     url(r'^blog/(?P<slug>\S+)$', 'website.views.view_post', name='view_post'),
 
-    # url(r'^about$', 'website.views.about', name='about'),
     # url(r'^resources/', include('resources.urls')),
     url(r'^contact$', 'website.views.contact', name='contact'),
     url(r'^thanks$', 'website.views.thanks', name='thanks'),
-
-    # url(r'form/$', 'website.views.comment_view', name='comment_view'),
 
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^feed/$', feed.LatestPosts(), name='feed'),
